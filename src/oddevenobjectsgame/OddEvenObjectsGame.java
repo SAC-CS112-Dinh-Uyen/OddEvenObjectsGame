b6a93f1098879b344955efbadbaf93e0a3486d37
Durendal <huynh_durendal@sac.edu>
2016-10-15 23:23:34 -0700

    Update OddEvenObjectsGame.java
    // Please add a method PlayGame to house the game logic.  Please do not use the Constructor to implement logic.
    // Constructors are meant to intialize values

--- src/oddevenobjectsgame/OddEvenObjectsGame.java
+++ src/oddevenobjectsgame/OddEvenObjectsGame.java
@@ -10,3 +10,6 @@ public class OddEvenObjectsGame
         myGame.displayMessage();
     }//End main
 }//end class OddEvenObjectsGame
+
+// Please add a method PlayGame to house the game logic.  Please do not use the Constructor to implement logic.
+// Constructors are meant to intialize values
